ab58bdce75285f7c423a137786d200f4dc0a85ef
rzjprogramador <rzjprogramador@gmail.com>
2022-04-18 13:38:15 -0300

    feat: buscando dados do backend via api graphql no front end react ts semelhante ao playground apollo

--- front_react/front_typegrap_01/src/App.tsx
+++ front_react/front_typegrap_01/src/App.tsx
@@ -1,7 +1,36 @@
+import { gql, useQuery } from '@apollo/client'
+
+type User = {
+  id: string
+  nome: string
+  idade: number
+}
+
+const GET_USER = gql`
+  query {
+    users {
+    id
+    nome
+    }
+  }
+`
 
 function App() {
+  const { data, loading } = useQuery<{ users: User[] }>(GET_USER)
+  console.log(data)
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <h1>Alo Mundo</h1>
+    <ul>
+      {data?.users.map(user => 
+        <li key={user.id}>
+          {user.nome}
+        </li>
+      )}
+    </ul>
   )
 }
 
