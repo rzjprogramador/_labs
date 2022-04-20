5b8cc4c5dea825eb11d5f1c2f7322c0a278848c7
rzjprogramador <rzjprogramador@gmail.com>
2022-04-20 15:01:45 -0300

    feat: utilizando a funcao vinda do servico -- injecao de dependencia

--- nest_js/nest_o1_encomendas_01/src/encomenda_test1/encomenda_test1.controller.ts
+++ nest_js/nest_o1_encomendas_01/src/encomenda_test1/encomenda_test1.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { EncomendaTest1Service } from './encomenda_test1.service';
 
 @Controller('encomenda-test1')
 export class EncomendaTest1Controller {
+  constructor(private readonly encomendaTest1Service: EncomendaTest1Service) {}
+
   @Get('rota1')
   alo(): string {
     return `Alo to no Controle 1 --`;
@@ -10,5 +13,11 @@ export class EncomendaTest1Controller {
   @Get('rota2')
   rota2(): string {
     return `Esta é a rota 2`;
+  }
+
+  // UTILIZANDO A FUNCAO VINDA DO SERVICO
+  @Get('rota-encomenda1')
+  funcaoServicalControle(): string {
+    return this.encomendaTest1Service.getFuncaoServical();
   }
 }
--- nest_js/nest_o1_encomendas_01/src/encomenda_test1/encomenda_test1.service.ts
+++ nest_js/nest_o1_encomendas_01/src/encomenda_test1/encomenda_test1.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EncomendaTest1Service {}
+export class EncomendaTest1Service {
+  getFuncaoServical(): string {
+    return `Retorno da funcao Servical`;
+  }
+}
