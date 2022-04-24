e89922865059d94a3118a650522a2f07027b0b13
rzjprogramador <rzjprogramador@gmail.com>
2022-04-24 00:40:57 -0300

    feat: configurando a classe prisma service

--- nest_js/nest_o1_encomendas_01/src/prisma.service.ts
+++ nest_js/nest_o1_encomendas_01/src/prisma.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  onModuleDestroy() {
+    // TODO
+    throw new Error('Method not implemented.');
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
