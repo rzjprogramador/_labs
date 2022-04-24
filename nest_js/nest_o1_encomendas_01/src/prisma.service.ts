1882ccc114efcda108b99fe1a3f27dc5faa66210
rzjprogramador <rzjprogramador@gmail.com>
2022-04-24 00:48:20 -0300

    feat: servico prisma principal o modulo na ordem o modulo connect conecta e o modulo destroy disconeta ..desta forma ele é um singleton

--- nest_js/nest_o1_encomendas_01/src/prisma.service.ts
+++ nest_js/nest_o1_encomendas_01/src/prisma.service.ts
@@ -1,16 +1,16 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  onModuleDestroy() {
-    // TODO
-    throw new Error('Method not implemented.');
-  }
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
