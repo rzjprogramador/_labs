04cf8f1e893ccb3552a26ccf7a2d9ac8a2578459
rzjprogramador <rzjprogramador@gmail.com>
2022-04-25 15:30:47 -0300

    feat: buscando dado por param id

--- nest_js/nest_o1_encomendas_01/src/encomenda_test1/encomenda_test1.controller.ts
+++ nest_js/nest_o1_encomendas_01/src/encomenda_test1/encomenda_test1.controller.ts
@@ -12,6 +12,17 @@ export class EncomendaTest1Controller {
     return this.encomendaTest1Service.pais({});
   }
 
+  // TRAZENDO VIA SERVICO :: LA DO PRISMA  :: POR PARAMS UNICO DADO PASSADO
+  // ACESSAR : http://localhost:3333/encomenda-test1/deletar-pai/3     >> O ULTIMO NUMERO É DO ID PASSADO
+  @Get('deletar-pai/:id')
+  async getDeletarPaiById(@Param('id') id: string): Promise<DeletarPai> {
+    return this.encomendaTest1Service.pai({
+      id: Number.parseInt(id),
+
+      // por no prisma o id ser inteiro apra auto-incrementar e no graphql ser string do ID convertemso para int aqui
+    });
+  }
+
   @Get('rota1')
   alo(): string {
     return `Alo to no Controle 1 --`;
