70f6a823b87885f1900ceea6ba4b39d9429f908e
rzjprogramador <rzjprogramador@gmail.com>
2022-05-24 09:26:09 -0300

    ok

--- node/Test_E2E_Prisma_01_base/src/modules/prodEncomenda/shared_module/router_prod_encomenda.ts
+++ node/Test_E2E_Prisma_01_base/src/modules/prodEncomenda/shared_module/router_prod_encomenda.ts
@@ -3,6 +3,5 @@ import { Router, Request, Response} from 'express'
 export const routerProdEncomenda = Router()
 
 routerProdEncomenda.get('/encomenda', (req: Request, res: Response) => {
-  console.log('Estou no encomendas')
-  res.send('Ola encomenda')
+  res.send('Ola encomenda 2')
 })
