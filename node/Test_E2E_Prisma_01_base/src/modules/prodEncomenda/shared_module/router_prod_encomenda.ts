7406ab68602e3b316db8ffa760c04dbc4f58d457
rzjprogramador <rzjprogramador@gmail.com>
2022-05-24 09:30:37 -0300

    fix: respondendo api

--- node/Test_E2E_Prisma_01_base/src/modules/prodEncomenda/shared_module/router_prod_encomenda.ts
+++ node/Test_E2E_Prisma_01_base/src/modules/prodEncomenda/shared_module/router_prod_encomenda.ts
@@ -3,5 +3,6 @@ import { Router, Request, Response} from 'express'
 export const routerProdEncomenda = Router()
 
 routerProdEncomenda.get('/encomenda', (req: Request, res: Response) => {
-  res.send('Ola encomenda 2')
+  // res.send('Ola encomenda 2')
+  return res.status(201).json({ message: 'Bom Dia'})
 })
