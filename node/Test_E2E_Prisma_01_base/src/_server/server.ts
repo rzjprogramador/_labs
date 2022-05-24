fc67a79cdc0528b9360c7d47e9cda35fa9875bfc
rzjprogramador <rzjprogramador@gmail.com>
2022-05-24 09:24:05 -0300

    fix: modularizada rota express

--- node/Test_E2E_Prisma_01_base/src/_server/server.ts
+++ node/Test_E2E_Prisma_01_base/src/_server/server.ts
@@ -1,6 +1,14 @@
 import 'module-alias/register'
+import express from 'express'
 
+// import { app } from '@/_server/app'
 import { app } from './app'
+// import routes  from '../routes'
+import { routes } from '../routes'
+
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, () => {
   console.log(`SERVER_ON :: 3333`)
---
+++ node/Test_E2E_Prisma_01_base/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { routerProdEncomenda } from '../modules/prodEncomenda/shared_module/router_prod_encomenda'
+import { Router } from 'express'
+
+
+export const routes = Router()
+
+// EXPORT TODOS CENTRALIZADORES DE ROTAS
+
+routes.use(routerProdEncomenda)
