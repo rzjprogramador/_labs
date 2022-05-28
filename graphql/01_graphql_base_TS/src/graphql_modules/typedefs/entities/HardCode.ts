aa33287470a04f6423b1d750590763e75eb4802b
rzjprogramador <rzjprogramador@gmail.com>
2022-05-28 16:05:21 -0300

    ok

--- graphql/01_graphql_base_TS/src/graphql_modules/typedefs/entities/HardCode.ts
+++ graphql/01_graphql_base_TS/src/graphql_modules/typedefs/entities/HardCode.ts
@@ -6,14 +6,8 @@ export const hardCode = gql`
 
 type Query {
     texto: String
-    # dadoObjUm: DadoObjUm
   }
 
-  # A ENTIDADE PODE FAZER : CONSULTA :: MODELAR A CONSULTA
-  # MODELE O QUE ELA PODE FAZER E RESOLVA NO RESOLVER
-  # type Query {
-  #   getHello: Hello
-  # }
 
-
-`//
+`
+//
