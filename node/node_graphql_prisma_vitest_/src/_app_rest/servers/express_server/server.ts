d0d3a46f0a8eb5fc417d3ab6c7412c6a326c3b0b
rzjprogramador <rzjprogramador@gmail.com>
2022-06-01 14:50:16 -0300

    ok

--- node/node_graphql_prisma_vitest_/src/_app_rest/servers/express_server/server.ts
+++ node/node_graphql_prisma_vitest_/src/_app_rest/servers/express_server/server.ts
@@ -1,8 +1,6 @@
 // @ts-ignore
 import 'module-alias/register'
 import express from 'express'
-import { graphqlHTTP } from 'express-graphql'
-import schema from '../../graphql_central/schema'
 
 import { app } from './app'
 import { routes } from './routes_express'
